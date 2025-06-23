d889dde4bd4485fd5ab7709437c2229acb14b658
Ahmad <ahmadkhaefi@outlook.com>
2025-06-23 13:02:57 +0330

    tweaked courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,11 +14,11 @@ const Part = ({part, exercises}) => {
 
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
     return (
-        <>
-        <Part part={part1} exercises={exercises1}/>
-		<Part part={part2} exercises={exercises2}/>
-		<Part part={part3} exercises={exercises3}/>
-        </>
+        <div>
+			<Part part={part1} exercises={exercises1}/>
+			<Part part={part2} exercises={exercises2}/>
+			<Part part={part3} exercises={exercises3}/>
+        </div>
     )
 }
 
